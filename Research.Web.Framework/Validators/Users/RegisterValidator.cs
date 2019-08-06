6a82fab7e5cbc56b25feb33e4c3821e4166a40e0
chai <th.sakchai@gmail.com>
2019-08-06 09:56:38 +0800

    update ID Card validate

--- Research.Web.Framework/Validators/Users/RegisterValidator.cs
+++ Research.Web.Framework/Validators/Users/RegisterValidator.cs
@@ -30,6 +30,7 @@ namespace Research.Web.Validators.Users
                 .EmailAddress()
                 .WithMessage("ระบุอีเมล!");
 
+
             RuleFor(x => x.Email).Must((x, context) =>
             {
                 return IsDuplicateEmailChecked(x, userService);
@@ -45,15 +46,19 @@ namespace Research.Web.Validators.Users
                 .Length(13)
                 .WithMessage("ระบุเลขประจำตัวประชาชนจำนวน 13 หลัก!");
 
+            RuleFor(x => x.IDCard)
+                .Matches(@"^\d{7}$")
+                .WithMessage("ระบุเลขประจำตัวประชาชนเป็นตัวเลขเท่านั้น!");
+
             RuleFor(x => x.IDCard).Must((x, context) =>
             {
                 return IsDuplicateIDCardChecked(x, userService);
             }).WithMessage("เลขประจำตัวประชาชนซ้ำในระบบ กรุณาระบุเลขประจำตัวประชาชนใหม่อีกครั้ง!");
 
-            //RuleFor(x => x.IDCard).Must((x, context) =>
-            //{
-            //    return IsValidIDCard(x);
-            //}).WithMessage("เลขประจำตัวประชาชน ไม่ถูกต้อง!");
+            RuleFor(x => x.IDCard).Must((x, context) =>
+            {
+                return IsValidIDCard(x);
+            }).WithMessage("เลขประจำตัวประชาชนไม่ถูกต้อง กรุณาระบุเลขประจำตัวประชาชนใหม่อีกครั้ง!");
 
 
             SetDatabaseValidationRules<User>(dbContext);
@@ -74,17 +79,31 @@ namespace Research.Web.Validators.Users
 
         private bool IsValidIDCard(RegisterModel model)
         {
+            int sum = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                string pos = ((char)model.IDCard[i]).ToString();
+                sum += int.Parse(pos) * (13 - i);
+            }
+            string pos12 = ((char) model.IDCard[12]).ToString();
+
+            return ((11 - sum % 11) % 10 == int.Parse(pos12));
+
+        }
+
+        private bool IsValidIDCard2(RegisterModel model)
+        {
             long id = long.Parse(model.IDCard);
-            long @base = 100000000000l; //สร้างตัวแปร เพื่อสำหรับให้หารเพื่อเอาหลักที่ต้องการ
+            long _base = 100000000000l; //สร้างตัวแปร เพื่อสำหรับให้หารเพื่อเอาหลักที่ต้องการ
             int basenow; //สร้างตัวแปรเพื่อเก็บค่าประจำหลัก
             int sum = 0; //สร้างตัวแปรเริ่มตัวผลบวกให้เท่ากับ 0
             for (int i = 13; i > 1; i--)
             { //วนรอบตั้งแต่ 13 ลงมาจนถึง 2
-                basenow = (int)Math.Floor((decimal)id / @base); //หาค่าประจำตำแหน่งนั้น ๆ
-                id = id - basenow * @base; //ลดค่า id ลงทีละหลัก
+                basenow = (int)Math.Floor((decimal)id / _base); //หาค่าประจำตำแหน่งนั้น ๆ
+                id = id - basenow * _base; //ลดค่า id ลงทีละหลัก
                 Console.WriteLine(basenow + "x" + i + " = " + (basenow * i)); //แสดงค่าเมื่อคูณแล้วของแต่ละหลัก
                 sum += basenow * i; //บวกค่า sum ไปเรื่อย ๆ ทีละหลัก
-                @base = @base / 10; //ตัดค่าที่ใช้สำหรับการหาเลขแต่ละหลัก
+                _base = _base / 10; //ตัดค่าที่ใช้สำหรับการหาเลขแต่ละหลัก
             }
             Console.WriteLine("Sum is " + sum); //แสดงค่า sum
             int checkbit = (11 - (sum % 11)) % 10; //คำนวณค่า checkbit
